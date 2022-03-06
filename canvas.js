44c969b01867607d0cef4dd1664e049abd6242f0
Alire Jafari <asjn3e@gmail.com>
2022-03-07 01:28:46 +0330

    fix canvas bugs & add logo container

--- canvas.js
+++ canvas.js
@@ -66,11 +66,115 @@ canvas.width=document.querySelector(".hero").scrollWidth;
 // }
 // animate();
 
+// let candles=[];
+
+// const mouse={
+//     latest_x:undefined,
+//     latest_y:undefined,
+//     x:undefined,
+//     y:undefined
+// }
+// class candlesBase{
+//     constructor(x,width){
+//         this.x=x;
+//         this.y=canvas.height;
+//         this.width=width;
+//         this.height=0;
+//         this.color="#5ae2dc";
+//     }   
+//     update(newColor){
+//         const normalHeight=((canvas.height - mouse.y) * - 1  ) + (canvas.height / 3);
+        
+//         if(normalHeight < -(canvas.height / 4)  ){
+//         this.height=-(canvas.height / 4)
+//         }else{
+//             this.height=normalHeight;
+
+//         }
+//         this.color=newColor;
+//     }
+//     draw(){
+//         ctx.fillStyle=this.color;
+//         ctx.fillRect(this.x,this.y,this.width,this.height);
+//     }
+// }
+
+// const candlesWidth=canvas.width / 75;
+// function init(){
+//     for (let index = 0; index < 75; index++) {
+//         candles.push(new candlesBase(index * candlesWidth,candlesWidth - candlesWidth / 2.3));
+//     }
+// }
+// init();
+
+// function updateCandles(){
+//     candles.forEach((element,index)=>{
+//         if(element.x < mouse.x && element.x + candlesWidth > mouse.x){
+//             if (mouse.x > mouse.latest_x && mouse.x > mouse.latest_x + candlesWidth){
+//                 // console.log(mouse)
+//                 if(mouse.y > mouse.latest_y){
+//                     // console.log("hi2")
+//                     element.update("#f02d83");
+//                     element.draw();
+//                 }else{
+//                     element.update("#5ae2dc");
+//                     element.draw();
+//                 }
+
+//                 mouse.latest_y=element.height + (canvas.height / 3) * 2 
+
+//             }else if(mouse.x < mouse.latest_x && mouse.x < mouse.latest_x + candlesWidth){
+//                 if(mouse.y > mouse.latest_y){
+//                     // console.log("hi2")
+//                     element.update("#f02d83");
+//                     element.draw();
+//                 }else{
+//                     element.update("#5ae2dc");
+//                     element.draw();
+//                 }
+//                 mouse.latest_y=element.height + (canvas.height / 3) * 2
+                
+//             }else{
+
+//                 if(mouse.y > mouse.latest_y){
+                        
+//                     element.update("#f02d83");
+//                     element.draw();
+//                 }else{
+                    
+//                     element.update("#5ae2dc");
+//                     element.draw();
+//                 }  
+//             }
+            
+//             mouse.latest_x=element.x;
+
+//         }
+//         else{
+//             element.draw();
+//         }
+//     })
+// }
+
+// document.addEventListener("mousemove",(e)=>{
+//     mouse.y=e.y;
+//     mouse.x=e.x - document.querySelector(".navbar").scrollWidth;
+//     // console.log(mouse)
+// })
+
+
+// function animate(){
+//     ctx.clearRect(0,0,canvas.width,canvas.height);
+//     updateCandles();
+
+//     requestAnimationFrame(animate);
+// }
+// animate()
+
+
 let candles=[];
 
 const mouse={
-    latest_x:undefined,
-    latest_y:undefined,
     x:undefined,
     y:undefined
 }
@@ -79,79 +183,68 @@ class candlesBase{
         this.x=x;
         this.y=canvas.height;
         this.width=width;
-        this.height=0;
+        this.height=Math.random() * -180;
+        this.lineHeight=(Math.random() * - 20) + canvas.height + this.height
         this.color="#5ae2dc";
+        this.modificationNumber=undefined;
     }   
     update(newColor){
-        const normalHeight=((canvas.height - mouse.y) * - 1  ) + (canvas.height / 3);
-        
-        if(normalHeight < -(canvas.height / 2.7)  ){
-        this.height=-(canvas.height / 2.7)
-        }else{
-            this.height=normalHeight;
-
-        }
+        // this.height=Math.random() * -200;
         this.color=newColor;
     }
     draw(){
         ctx.fillStyle=this.color;
         ctx.fillRect(this.x,this.y,this.width,this.height);
+        ctx.strokeStyle=this.color
+        ctx.lineWidth=1
+        ctx.beginPath();
+        ctx.moveTo(this.x+this.width / 2,this.y);
+        ctx.lineTo(this.x+this.width / 2,this.lineHeight);
+        ctx.stroke();
+        ctx.closePath()
     }
 }
 
-const candlesWidth=canvas.width / 60;
+const candlesWidth=canvas.width / 80;
 function init(){
-    for (let index = 0; index < 60; index++) {
+    for (let index = 0; index < 80; index++) {
         candles.push(new candlesBase(index * candlesWidth,candlesWidth - candlesWidth / 2.3));
     }
 }
 init();
-
+let modificationCounter=0;
 function updateCandles(){
     candles.forEach((element,index)=>{
-        if(element.x < mouse.x && element.x + candlesWidth > mouse.x){
-            if (mouse.x > mouse.latest_x && mouse.x > mouse.latest_x + candlesWidth){
-                // console.log(mouse)
-                if(mouse.y > mouse.latest_y){
-                    // console.log("hi2")
-                    element.update("#f02d83");
-                    element.draw();
-                }else{
-                    element.update("#5ae2dc");
-                    element.draw();
-                }
-
-                mouse.latest_y=element.height + (canvas.height / 3) * 2 
-
-            }else if(mouse.x < mouse.latest_x && mouse.x < mouse.latest_x + candlesWidth){
-                if(mouse.y > mouse.latest_y){
-                    // console.log("hi2")
-                    element.update("#f02d83");
-                    element.draw();
-                }else{
-                    element.update("#5ae2dc");
-                    element.draw();
-                }
-                mouse.latest_y=element.height + (canvas.height / 3) * 2
+        if(element.x < mouse.x && element.x + candlesWidth > mouse.x && index != 0){
+            if(element.modificationNumber!=modificationCounter){
+                element.height=Math.random() * -180;
+                element.lineHeight=(Math.random() * - 50) + canvas.height + element.height
                 
-            }else{
-
-                if(mouse.y > mouse.latest_y){
-                        
+                if(element.height > candles[index -1].height){
                     element.update("#f02d83");
-                    element.draw();
-                }else{
-                    
+                }
+                else{
                     element.update("#5ae2dc");
-                    element.draw();
-                }  
+    
+                }
+                modificationCounter++;
+                element.modificationNumber=modificationCounter;
+                element.draw();
             }
-            
-            mouse.latest_x=element.x;
-
         }
         else{
+            if(index != 0){
+            if(element.height > candles[index -1].height){
+                element.update("#f02d83");
+            }
+            else{
+                element.update("#5ae2dc");
+
+            }
             element.draw();
+        }else{
+            element.draw();
+        }
         }
     })
 }
